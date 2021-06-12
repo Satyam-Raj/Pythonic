875c82875811d06a20ad6092e6412d87d9eee73b
Satyam Raj <44699922+Satyam-Raj@users.noreply.github.com>
2021-06-12 22:45:12 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+import wikipedia
+
+
+def Search(s):
+
+	wikipedia.page(s)
+
+	Title			= 	(wikipedia.page(s).title)
+	Url 			=	(wikipedia.page(s).url)
+	Content			=	(wikipedia.page(s).content)
+	References		=	(wikipedia.page(s).references)
+	Related_links	=	(wikipedia.page(s).links)
+
+	return Title, Url, Content, References, Related_links
+
+
+
+
